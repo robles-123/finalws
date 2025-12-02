e240650144bd7edc920c3b27d41ef6cd07a8bfe8
robles-123 <robles-123@example.com>
2025-12-03 01:12:27 +0800

    build(vite): increase chunkSizeWarningLimit to 1500 kB

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Suppress warning for chunks larger than 1500 kB
+  },
 })
